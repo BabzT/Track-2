676264cc69f7dec11c145f9a018a34589bdeda9b
BabzT <babatundehezekiah7@gmail.com>
2026-05-06 13:02:18 -0400

    update

--- src/controllers/users.ts
+++ src/controllers/users.ts
@@ -5,14 +5,12 @@ export const getMe = async (req: Request, res: Response) => {
   try {
     const response = await usersService.fetchMe(req.user?.id as string);
 
-    if (!response.success) {
-      return console.log("User not found");
+    if (response.success) {
+      return res.status(200).send({
+        message: "User fetched successfully",
+        data: response.data,
+      });
     }
-
-    return res.status(200).send({
-      message: "User fetched successfully",
-      data: response.data,
-    });
   } catch (error) {
     console.log(error);
     res.status(500).json({ message: "Internal server error" });
